ad549c92db8c39cdc1493224e815ff66b2575b09
ftuedu <sccma@qq.com>
2021-10-12 13:48:22 +0800

    修改hook

--- hook/index.php
+++ hook/index.php
@@ -6,7 +6,7 @@ $secret = "ftuedu112233";
 // Path to your respostory on your server.
 // e.g. "/var/www/respostory"
 // 项目地址
-$path = "/www/wwwroot/sanay.cn";
+$path = "/www/wwwroot/w7.pplu.site";
 
 // Headers deliveried from GitHub
 $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
